a7e934ac64964723baa1866483d028b6f0659f92
rz <rz19960718@gmail.com>
2021-04-09 01:08:42 +0800

    chore

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
-### 支持列排序和选择性显示/隐藏列的 Table
+# Ant Design Table With Superpower
 
-基于 Ant Design 的 Table 组件，支持动态显示/隐藏列以及对要显示的列进行排序，使用 react-beautiful-dnd 实现
+以不改变 Ant Deisgn Table 的 API 为前提, 为 Table 组件增加列排序和显示/隐藏列的功能
 
-#### How to use
+Powered By react-beautiful-dnd
 
-1. 安装依赖: yarn install
+查看 Demo : http://widgets.realrz.com/ant-table-dnd
 
-2. 启动项目: yarn start
+#### 如何使用
+
+1. 安装依赖: yarn
+
+2. 启动项目: yarn run start
 
 3. 点击右上角设置图标，水平拖动列名来显示/隐藏列, 垂直拖动来改变列的排序, 点击 OK 保存设定
 
 #### 自定义样式
 
-你可以修改源代码进行自定义样式，比如背景色，字体大小，margin，padding 等
+你可以修改源代码进行自定义 Modal 样式，比如背景色，字体大小，margin，padding 等
--- src/index.js
+++ src/index.js
@@ -3,18 +3,31 @@ import ReactDOM from "react-dom";
 import AntTableDnd from "./AntTableDnd";
 import columns from "./Columns";
 import rows from "./Rows";
+import { Typography } from "antd";
+import { ArrowDownOutlined } from "@ant-design/icons";
 import "antd/dist/antd.css";
+
+const { Title } = Typography;
 
 const App = () => {
   return (
     <div style={{ display: "flex", justifyContent: "center", padding: 50 }}>
-      <AntTableDnd
-        columns={columns}
-        bordered
-        pagination={false}
-        dataSource={rows}
-        rowKey="name"
-      />
+      <div>
+        <Title level={2}>Ant Design Table With Superpower</Title>
+        <div style={{ display: "flex", justifyContent: "flex-end" }}>
+          <Title level={4} style={{ marginRight: 20 }}>
+            点击设置然后进行拖拽 (上下拖拽可以改变列的显示顺序，左右拖拽可以显示/隐藏列)
+          </Title>
+          <ArrowDownOutlined style={{ fontSize: 30 }} />
+        </div>
+        <AntTableDnd
+          columns={columns}
+          bordered
+          pagination={false}
+          dataSource={rows}
+          rowKey="name"
+        />
+      </div>
     </div>
   );
 };
